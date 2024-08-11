9fa8a7c00a35c939dcc04b298f6704a123362a74
Priyansh <im1706@gmail.com>
2024-08-11 16:13:41 +0530

    Updated career data

--- src/Pages/CareerData.js
+++ src/Pages/CareerData.js
@@ -1,8 +1,17 @@
 const CareerStats = [
     {
         "corp":"Cloudera Inc.",
-        "time":"August 2022 - Present",
-        "workDone":"• Developed a platform to automate computing cloud platform (Azure, AWS, GCP) costs and minimise wastage of resources. ",
+        "time":"Jan 2024 - July 2024",
+        "workDone":`• Led development and planning efforts to implement a resource provisioning system via ansible and django. <br/>• Streamlined resource allocation processes, ensuring efficient utilization and adherence to allocated quotas.
+         <br/>• Developed Helm charts, hooks, and migration workflows to facilitate seamless deployment and management of applications, services on Rancher Kubernetes clusters`,
+        "location":"Bengaluru, India",
+        "postiion":"Software Engineer - 2"
+    },
+    {
+        "corp":"Cloudera Inc.",
+        "time":"August 2022 - Dec 2023",
+        "workDone":`• Leading organization-wide application built using golang and reactJS for conducting cloud resource cleanup and cost analysis, resulting in savings of approximately $5k per month.Providing feature additions and support for the same.
+         <br/>• Led deployment, configuration, and automation of Prometheus,Pushgateway Grafana, Blackbox and AlertManager ensuring efficient monitoring and alerting of clusters.`,
         "location":"Bengaluru, India",
         "postiion":"Software Engineer - 1"
     },
